f03a0e49b84f1e6465f59031ce883806125006f7
Trung Tran <trung.truman.tran@gmail.com>
2019-05-12 23:56:13 -0400

    Add unit toggle

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,34 @@ import React from 'react';
 class Info extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      unitFormat: 'F'
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.displayTemp = this.displayTemp.bind(this);
   }
+
+  // Display temperature in Fahrenheit or Celsius depending on the toggle
+  displayTemp() {
+    if (this.state.unitFormat === 'F') {
+      return this.props.weatherData.main.temp;
+    } else {
+      return (
+        (Number(this.props.weatherData.main.temp) - 32) *
+        (5 / 9)
+      ).toFixed(2);
+    }
+  }
+
+  handleClick() {
+    if (this.state.unitFormat === 'F') {
+      this.setState({ unitFormat: 'C' });
+    } else {
+      this.setState({ unitFormat: 'F' });
+    }
+  }
+
   render() {
     return (
       <div className="weather">
@@ -11,7 +38,7 @@ class Info extends React.Component {
           <div>
             <div className="city-name">{this.props.weatherData.name}</div>
             <div className="temp">
-              Temperature: {this.props.weatherData.main.temp} °F
+              Temperature: {this.displayTemp()} °{this.state.unitFormat}
             </div>
             <div className="humidity">
               Humidity: {this.props.weatherData.main.humidity}%
@@ -23,6 +50,9 @@ class Info extends React.Component {
         ) : (
           <div>Invalid Input. Please Try Again</div>
         )}
+        <button className="unit-toggle" onClick={this.handleClick}>
+          {this.state.unitFormat === 'F' ? 'Celsius' : 'Fahrenheit'}
+        </button>
       </div>
     );
   }
